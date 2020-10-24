4c5ef72a133df2bba804395f19f6a2ae2508370a
Nate <nathan.saunders@endlas.com>
2020-10-24 07:33:36 -0600

    Fixed issue where com port was opening but no set bits would ever input. Program no longer renders an image after 5 serial events. It now just cycles through reading the incoming bytes and printing them to the console.

--- py/laser_safety_runner/main.py
+++ py/laser_safety_runner/main.py
@@ -11,32 +11,38 @@ import serial
 IMAGE_DIR = "resources/"
 IMAGE_JPG = "fault.jpg"
 IMAGE_TO_SHOW = IMAGE_DIR + IMAGE_JPG
-COM_PORT = 'COM3'
+COM_PORT = 'COM5'
 
+isInit = True
 
 # int main()
 if __name__ == '__main__':
-    try:
-        ser = serial.Serial(port=COM_PORT, baudrate=9600, bytesize=8, timeout=2)
-        input_byte0 = ser.read(1)
-        print(bin(int.from_bytes(input_byte0, byteorder="big", signed=False)))
 
-        input_byte1 = ser.read(1)
-        print(bin(int.from_bytes(input_byte1, byteorder="big", signed=False)))
+    while True:
+        try:
+            if isInit is True:
+                isInit = False
+                ser = serial.Serial(port=COM_PORT, baudrate=9600, bytesize=8, timeout=2)
 
-        input_byte2 = ser.read(1)
-        print(bin(int.from_bytes(input_byte2, byteorder="big", signed=False)))
+            input_byte0 = ser.read(1)
+            print(bin(int.from_bytes(input_byte0, byteorder="big", signed=False)))
 
-        input_byte3 = ser.read(1)
-        print(bin(int.from_bytes(input_byte3, byteorder="big", signed=False)))
+            input_byte1 = ser.read(1)
+            print(bin(int.from_bytes(input_byte1, byteorder="big", signed=False)))
 
-        input_byte4 = ser.read(1)
-        print(bin(int.from_bytes(input_byte4, byteorder="big", signed=False)))
+            input_byte2 = ser.read(1)
+            print(bin(int.from_bytes(input_byte2, byteorder="big", signed=False)))
 
-        manip.display_image(IMAGE_TO_SHOW)
+            input_byte3 = ser.read(1)
+            print(bin(int.from_bytes(input_byte3, byteorder="big", signed=False)))
 
-    except serial.SerialException:
-        print("Unable to open COM port: " + COM_PORT)
+            input_byte4 = ser.read(1)
+            print(bin(int.from_bytes(input_byte4, byteorder="big", signed=False)))
 
+            print("\n")
+            # manip.display_image(IMAGE_TO_SHOW)
+
+        except serial.SerialException:
+            print("Unable to open COM port: " + COM_PORT)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
