fdcfbce93b31c7abdc2664c6febf1b61fe93dd5d
Nate <nathan.saunders@endlas.com>
2020-10-28 08:11:56 -0600

    Fixed issue where sometimes waiting for response screen wouldn't render when it was supposed to.

--- py/laser_safety_runner/main.py
+++ py/laser_safety_runner/main.py
@@ -7,34 +7,19 @@ import byte_manip as b_manip
 import serial
 import constants as c
 import pygame
+import platform
 from pygame.locals import *
 
 # default image path to no-load
 DEFAULT_IMG = c.NO_LOAD_IMG
 
 
-def display_no_input_device():
-    pass
-
-
-def main(num_loops, canvas, is_pygame_init):
-    # flag so we don't try and open a port twice
-    is_com_port_open = False
+def main(canvas, is_com_port_open):
     # init vars
     ser = ""
     img = DEFAULT_IMG
     # initialize pygame
-    pygame.init()
-    pygame.display.set_caption('LASER SAFETY RUNNER')
-    if is_com_port_open is False:
-        if num_loops % 10000 == 0:
-            canvas.fill(c.SKY_BLUE)
-            font = pygame.font.Font('freesansbold.ttf', 32)
-            text = font.render('Waiting for input device reply...', True, c.LIGHT_BLUE, c.NAVY)
-            text_rect = text.get_rect(center=(c.DISPLAY_WIDTH / 2.0, c.DISPLAY_HEIGHT / 2.0))
-            canvas.blit(text, text_rect)
-            # render changes
-            pygame.display.update()
+
     py_img_last = ""
     # void loop()
     while True:
@@ -61,7 +46,7 @@ def main(num_loops, canvas, is_pygame_init):
                     is_com_port_open = True
             #  port failed to open
             except serial.SerialException:
-                print("Unable to open COM port: " + c.COM_PORT)
+                # print("Unable to open COM port: " + c.COM_PORT)
                 return
         # successful port open, so start loop
         elif is_com_port_open is True:
@@ -71,7 +56,12 @@ def main(num_loops, canvas, is_pygame_init):
             try:
                 byte_arr = ser.read(5)
             except serial.serialutil.SerialException:
-                is_com_port_open = False
+                print("SerialException")
+                display_waiting_for_reply(canvas)
+                pygame.display.update()
+                main(canvas, False)
+                return
+
             ser.flushInput()
             # make sure input is valid
             if b_manip.is_input_valid(byte_arr):
@@ -101,13 +91,40 @@ def main(num_loops, canvas, is_pygame_init):
             pygame.display.update()
 
 
+####################################################################
+# Name: determine_platform
+# Description: method that sets COM_PORT to system dependent syntax
+####################################################################
+def determine_platform():
+    platform_info = platform.uname()
+    if platform_info.system == 'Windows':
+        c.COM_PORT = "COM5"
+    elif platform_info.system == 'Raspbian':
+        c.COM_PORT = "/dev/ttyS0"
+    print(platform_info)
+
+
 # int main()
+def display_waiting_for_reply(display_canvas):
+    display_canvas.fill(c.SKY_BLUE)
+    pygame.init()
+    pygame.display.set_caption('LASER SAFETY RUNNER')
+    font = pygame.font.Font('freesansbold.ttf', 40)
+    text = font.render('Waiting for input device reply...', True, c.LIGHT_BLUE, c.NAVY)
+    text_rect = text.get_rect(center=(int(c.DISPLAY_WIDTH / 2), int(c.DISPLAY_HEIGHT / 2)))
+    display_canvas.blit(text, text_rect)
+
+
 if __name__ == '__main__':
+    determine_platform()
     val = 99999
     index = 1
     canvas = pygame.display.set_mode((c.DISPLAY_WIDTH, c.DISPLAY_HEIGHT), pygame.FULLSCREEN)
-    main(val + index, canvas, False)
+    display_waiting_for_reply(canvas)
+    # render changes
+    pygame.display.update()
+    main(canvas, False)
     while True:
         index += 1
-        main(val+index, canvas, False)
+        main(canvas, False)
 
